9ecc83369e15fa68cdcd2ecf9fde720f8306e71c
csong202 <christina.song202@gmail.com>
2022-08-14 21:43:04 -0400

    add/remove regex pinned messages pattern

--- main.go
+++ main.go
@@ -4,21 +4,21 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"regexp"
 	"strconv"
 	"strings"
 
 	"discord_bots/bot2/config"
 	"discord_bots/bot2/utils"
 
-	// "./config"
-	// "./utils"
-
 	"github.com/bwmarrin/discordgo"
 )
 
 var (
 	BotToken string
-	CMD_SYMB = "~"
+	BOT_PREFIX = "~"
+	searchParams map[string]string
+	pinPatterns = make([]string, 0, 20)
 )
 
 func main() {
@@ -29,43 +29,21 @@ func main() {
 	utils.HandleError(err)
 	fmt.Println("new session")
 
+	// variable initialization
+	searchParams = make(map[string]string)
+	searchParams["from"] = "" // TODO autocomplete
+	searchParams["has"] = "" // TODO for has and in: popup with options
+	searchParams["in"] = ""
+	
+	// TODO during param. somehow make calendar pop up
+
+	// handlers
+
 	session.AddHandler(func (s *discordgo.Session, r *discordgo.Ready) {
 		fmt.Println("Bot is ready")
 	})
 
-	session.AddHandler(func (s *discordgo.Session, msg *discordgo.MessageCreate) {
-		if strings.Contains(msg.Content, CMD_SYMB) {
-			if (strings.Contains(msg.Content, CMD_SYMB + "thread")) {
-				threadArgs := strings.Split(msg.Content, " ")
-				var archiveDuration int
-				if (len(threadArgs) < 2) {
-					s.ChannelMessageSendReply(msg.ChannelID, "You must include the thread name!", msg.Reference())
-					return
-				}
-				if (len(threadArgs) > 2) {
-					var err error
-					archiveDuration, err = strconv.Atoi(threadArgs[2])
-					if (err != nil) {
-						s.ChannelMessageSendReply(msg.ChannelID, "Archive duration must be an integer!", msg.Reference())
-						return
-					}
-					if (!utils.Contains(utils.ValidArchiveDurations, archiveDuration)) {
-						s.ChannelMessageSendReply(msg.ChannelID, "Archive duration must be one of (1440, 60, 4320, 10080).", msg.Reference())
-						return
-					}
-				} else {
-					archiveDuration = 60 * 24
-				}
-				_, err := s.MessageThreadStart(msg.ChannelID, msg.ID, threadArgs[1], archiveDuration)
-				if err != nil {
-					return
-				}
-			} else {
-				s.ChannelMessageSendReply(msg.ChannelID, "not a valid command", msg.Reference())
-				// TODO: show help page
-			}
-		}
-	})
+	session.AddHandler(messageHandler)
 
 	session.Identify.Intents = discordgo.MakeIntent(discordgo.IntentsAllWithoutPrivileged)
 
@@ -76,5 +54,90 @@ func main() {
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, os.Interrupt)
 	<- stop
-	fmt.Println("Bot2 says bye-bye")
+	fmt.Println("Bot2 is going to sleep")
+}
+
+func messageHandler(s *discordgo.Session, msg *discordgo.MessageCreate) {
+	if len(msg.Content) > 0 && string(msg.Content[0]) == BOT_PREFIX {
+		if (strings.Contains(msg.Content, BOT_PREFIX + "thread")) {
+			threadArgs := strings.Split(msg.Content, " ")
+			var archiveDuration int
+			if (len(threadArgs) < 2) {
+				s.ChannelMessageSendReply(msg.ChannelID, "You must include the thread name!", msg.Reference())
+				return
+			}
+			if (len(threadArgs) > 2) {
+				var err error
+				archiveDuration, err = strconv.Atoi(threadArgs[2])
+				if (err != nil) {
+					s.ChannelMessageSendReply(msg.ChannelID, "Archive duration must be an integer!", msg.Reference())
+					return
+				}
+				if (!utils.ContainsInt(utils.ValidArchiveDurations, archiveDuration)) {
+					s.ChannelMessageSendReply(msg.ChannelID, "Archive duration must be one of (1440, 60, 4320, 10080).", msg.Reference())
+					return
+				}
+			} else {
+				archiveDuration = 60 * 24
+			}
+			_, err := s.MessageThreadStart(msg.ChannelID, msg.ID, threadArgs[1], archiveDuration)
+			utils.HandleError(err)
+		} else if (strings.Contains(msg.Content, BOT_PREFIX + "regex_search")) {
+			searchArgs := strings.Split(msg.Content, " ")
+			var pattern string
+			for _, arg := range searchArgs {
+				if (arg == BOT_PREFIX + "regexStr_search") {
+					continue
+				} else if strings.HasPrefix(arg, "from:") {
+					searchParams["from"] = arg[5:len(arg)-1]
+				} else if strings.HasPrefix(arg, "has:") {
+					searchParams["from"] = arg[5:len(arg)-1]
+				} else if strings.HasPrefix(arg, "in:") {
+					searchParams["from"] = arg[5:len(arg)-1]
+				} else {
+					pattern = arg
+				}
+			}
+
+			matched, err := regexp.MatchString(pattern, "...")
+			fmt.Println(matched)
+			fmt.Println(err)
+		} else if (strings.Contains(msg.Content, BOT_PREFIX + "regex_pin")) {
+			pinPatArgs := strings.Split(msg.Content, " ")
+			if len(pinPatArgs) < 3 {
+				s.ChannelMessageSendReply(msg.ChannelID, "Command must be in the form ~regex_pin <add|remove> <pattern>", msg.Reference())
+				return
+			}
+			if pinPatArgs[1] == "add" {
+				pinPatterns = append(pinPatterns, pinPatArgs[2])
+				fmt.Printf("Just added %s, now have %d pin patterns\n", pinPatArgs[2], len(pinPatterns))
+			} else if pinPatArgs[1] == "remove" {
+				err := utils.RemoveStr(&pinPatterns, pinPatArgs[2])
+				if err != nil {
+					s.ChannelMessageSend(msg.ChannelID, "This pattern was never added")
+				} else {
+					fmt.Printf("Just removed %s, now have %d pin patterns\n", pinPatArgs[2], len(pinPatterns))
+				}
+			} else {
+				s.ChannelMessageSendReply(msg.ChannelID, "invalid argument! must be add or remove", msg.Reference())
+				return
+			}
+		} else {
+			s.ChannelMessageSendReply(msg.ChannelID, "not a valid command", msg.Reference())
+			// TODO: show help page
+		} 
+	} else if checkMsgMatchPinPatterns(msg.Content) {
+		s.ChannelMessagePin(msg.ChannelID, msg.ID)
+	}
+}
+
+func checkMsgMatchPinPatterns(msg string) bool {
+	for _, pattern := range pinPatterns {
+		if pattern == "" { continue }
+		if matched, _ := regexp.MatchString(pattern, msg); matched{
+			fmt.Printf("Message \"%s\" matched %s\n", msg, pattern)
+			return true
+		}
+	}
+	return false
 }
--- utils/utils.go
+++ utils/utils.go
@@ -1,6 +1,9 @@
 package utils
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 var ValidArchiveDurations []int = []int{1440, 60, 4320, 10080}
 
@@ -11,9 +14,47 @@ func HandleError(err error) {
 	}
 }
 
-func Contains(slice []int, value int) bool {
+func ContainsInt(slice []int, value int) bool {
 	for _, elem := range slice {
 		if (elem == value) { return true}
 	}
 	return false
+}
+
+func ContainsStr(slice []string, value string) bool {
+	for _, elem := range slice {
+		if (elem == value) { return true}
+	}
+	return false
+}
+
+func RemoveStr(slice *[]string, value string) error {
+	if !ContainsStr(*slice, value) {
+		return errors.New("Slice does not contain the value")
+	}
+
+	n := len(*slice) - 1
+	newSlice := make([]string, n)
+
+	idx := 0
+	if n > 0 {
+		for i, elem := range *slice {
+			if elem == value {
+				newSlice[i] = ""
+				idx = i
+				break
+			}
+		}
+	}
+
+	for i := idx; i < n; i++ {
+		newSlice[i] = (*slice)[i + 1]
+	}
+
+	*slice = make([]string, n)
+	for i := 0; i < n; i++ {
+		(*slice)[i] = newSlice[i]
+	}
+
+	return nil
 }
